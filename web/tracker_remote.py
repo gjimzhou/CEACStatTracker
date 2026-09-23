2a93ed5b3a2162ac101ea9919bd8edeba022804d
Jimmy <gjimzhou@gmail.com>
2026-09-22 23:12:52 -0400

    Align remote requests with serverless payload shape

--- web/tracker_remote.py
+++ web/tracker_remote.py
@@ -9,8 +9,8 @@ def _remote_url():
         raise RuntimeError("CEAC_REMOTE_URL is not configured")
     return URL
 
-def query_ceac_state_safe(loc, case_no, soup=None):
-    req = requests.post(_remote_url(), json=[[loc,case_no]], timeout=100)
+def query_ceac_state_safe(loc, case_no, pp_no="", surname=""):
+    req = requests.post(_remote_url(), json=[[loc, case_no, pp_no, surname]], timeout=100)
     ret = req.json()
     return ret[case_no]
 
